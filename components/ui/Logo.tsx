5a46889f12476838f18caf96f7313a5ae61e958b
jonahcui <jonahcui0@gmail.com>
2022-07-05 11:09:44 +0800

    [chore] fix client side exception.

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react'
+
 import Image from 'next/image'
 import ethereumLogo from 'public/ethereum_logo.png'
 
@@ -5,7 +7,9 @@ export const Logo: React.FC = () => {
   return (
     <div className="flex items-center text-lg font-semibold w-32 tracking-tight text-gray-900 dark:text-white">
       <span style={{ paddingRight: '2px' }}>evm</span>
-      <Image src={ethereumLogo} width={20} height={20} />
+      <Fragment>
+        <Image src={ethereumLogo} width={20} height={20} />
+      </Fragment>
       <span style={{ paddingLeft: '2px' }}>codes</span>
     </div>
   )
